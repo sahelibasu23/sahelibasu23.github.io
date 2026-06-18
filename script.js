9697c62154f2e32b8ef31437aa319d3034dfb9d1
SAHELI  BASU <26185528+MehaRima@users.noreply.github.com>
2026-06-19 04:17:13 +0530

    Add files via upload
    updated

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ document.querySelectorAll(".navlinks a").forEach(link => {
 });
 
 document.getElementById("year").textContent = new Date().getFullYear();
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add("show");
+  } else {
+    backToTop.classList.remove("show");
+  }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
